01e0385fa080db41cda7efe929365a59f5f8c0ea
Winterworks <mail@bramwinter.nl>
2020-12-27 11:49:47 +0100

    Add more title texts

--- webapp/app.js
+++ webapp/app.js
@@ -33,6 +33,16 @@ const titleTexts = [
     'browsers',
     'Git',
     'networks',
+    'medicine',
+    'streaming',
+    'email',
+    'design',
+    'media',
+    'drones',
+    'CDN',
+    'apps',
+    'sensors',
+    'news',
 ];
 // Shuffle the titleText order
 titleTexts.sort(() => Math.random() - 0.5);
